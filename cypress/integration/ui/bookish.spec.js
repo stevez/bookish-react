1f1270c1e5279e294bdfe72eea86f8f3709cc759
Steve Zhang <huigang.zhang@gmail.com>
2021-12-05 17:06:42 -0500

    make the first e2e test working

---
+++ cypress/integration/ui/bookish.spec.js
@@ -0,0 +1,8 @@
+/// <reference types="cypress" />
+
+describe('Bookish application', () => {
+  it('Visits the bookish', () => {
+    cy.visit('http://localhost:3000/')
+    cy.get('h2[data-test="heading"]').contains('Bookish')
+  });
+});
